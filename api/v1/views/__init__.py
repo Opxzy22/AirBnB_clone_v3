a3b5eb8549e080cdb1886851ba6e89629c32cd5f
opxzy22 <aopeyem84@gmail.com>
2023-10-30 00:36:07 +0100

    progress

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 from flask import Flask, Blueprint
 from api.v1.views import app_views
+from api.v1.views.index import *
 
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
---
+++ api/v1/views/index.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""create a route for the status"""
+from api.v1.views import app_views
+from flask import jsonify
+from model import storage
+
+
+@app_views.route('/status', methods=['get'])
+def get_status():
+    status = ["status": "ok"]
+    return jsonify(status)
+
+
+@app.route('/app/v1/stats', strict_slashes=False)
+def get_stat():
+    stats = {}
+    classes = {
+            "amenities": "Amenity",
+            "states": "State",
+            "places": "Place",
+            "reviews": "Review",
+            "users": "User",
+            "cities": "City"
+            }
+    for class_name, class_key in classes.items():
+        count = storage.count(class_name)
+        stats[class_key] = count
+    return jsonify(stat)
