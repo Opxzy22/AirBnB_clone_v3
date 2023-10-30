e72154d5882b90b740398feaa0f5f77909cb4c1f
opxzy22 <aopeyem84@gmail.com>
2023-10-30 02:44:43 +0000

    progress

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
-""" this module starts a flask application"""
+""" This module starts a flask application """
+import os
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
-from flask import Blueprint
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def tear_down(exception):
-    """close the current session"""
+def remove(exception):
+    """ removes the current session """
 
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(err):
-    """configures 404 error response """
+def not_found(e):
+    """ configures 404 error response """
 
-    return jsonify({"error": "Not found"}), err.code
+    return jsonify({"error": "Not found"}), e.code
 
 
 if __name__ == "__main__":
